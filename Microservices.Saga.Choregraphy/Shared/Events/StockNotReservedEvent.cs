611e1c3c2b9c05f8c66fe41aa077003d09128f6e
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2025-02-07 15:08:44 +0300

    update shared

--- Microservices.Saga.Choregraphy/Shared/Events/StockNotReservedEvent.cs
+++ Microservices.Saga.Choregraphy/Shared/Events/StockNotReservedEvent.cs
@@ -2,6 +2,7 @@
 {
     public class StockNotReservedEvent
     {
+        //Order API'nın ihtiyacı olan bilgiler Stokta sorun olduğunda Order API'ya bildirilecek.
         public Guid OrderId { get; set; }
         public Guid BuyerId { get; set; }
         public string Message { get; set; }
