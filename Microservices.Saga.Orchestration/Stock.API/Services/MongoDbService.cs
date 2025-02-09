1070023924ee10022989c6fbfc55984d5a85f250
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2025-02-09 23:24:35 +0300

    cleaned code

--- Microservices.Saga.Orchestration/Stock.API/Services/MongoDbService.cs
+++ Microservices.Saga.Orchestration/Stock.API/Services/MongoDbService.cs
@@ -4,12 +4,14 @@ namespace Stock.API.Services
 {
     public class MongoDbService
     {
-        readonly IMongoDatabase _database;
+        private readonly IMongoDatabase _database;
+
         public MongoDbService(IConfiguration configuration)
         {
             MongoClient client = new(configuration.GetConnectionString("MongoDB"));
             _database = client.GetDatabase("StockDB");
         }
+
         public IMongoCollection<T> GetCollection<T>() => _database.GetCollection<T>(typeof(T).Name.ToLowerInvariant());
     }
 }
