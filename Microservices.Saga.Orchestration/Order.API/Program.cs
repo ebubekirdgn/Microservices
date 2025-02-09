2fafa11182026eaa3bce34baf973efdac37b6f4b
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2025-02-09 22:52:09 +0300

    update

--- Microservices.Saga.Orchestration/Order.API/Program.cs
+++ Microservices.Saga.Orchestration/Order.API/Program.cs
@@ -1,34 +1,46 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Order.API.Context;
+using Order.API.ViewModels;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddMassTransit(configurator =>
+{
+    configurator.UsingRabbitMq((context, _configure) =>
+    {
+        _configure.Host(builder.Configuration["RabbitMQ"]);
+    });
+});
+
+builder.Services.AddDbContext<OrderDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServer")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+app.MapPost("/create-order", async (CreateOrderVM model, OrderDbContext context) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    Order.API.Models.Order order = new()
+    {
+        BuyerId = model.BuyerId,
+        CreatedDate = DateTime.UtcNow,
+        OrderStatus = Order.API.Enums.OrderStatus.Suspend,
+        TotalPrice = model.OrderItems.Sum(oi => oi.Count * oi.Price),
+        OrderItems = model.OrderItems.Select(oi => new Order.API.Models.OrderItem
+        {
+            Price = oi.Price,
+            Count = oi.Count,
+            ProductId = oi.ProductId,
+        }).ToList(),
+    };
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    await context.Orders.AddAsync(order);
+    await context.SaveChangesAsync();
 });
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
