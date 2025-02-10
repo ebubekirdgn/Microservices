742dd9dfee963a9729a0ae3ee39c9d1b3043fb06
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2025-02-10 22:50:55 +0300

    add consumers in Stock.API

---
+++ Microservices.Saga.Orchestration/Stock.API/Consumers/StockRollbackMessageConsumer.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using MongoDB.Driver;
+using Shared.Messages;
+using Stock.API.Services;
+
+namespace Stock.API.Consumers
+{
+    public class StockRollbackMessageConsumer(MongoDbService mongoDbService) : IConsumer<StockRollbackMessage>
+    {
+        public async Task Consume(ConsumeContext<StockRollbackMessage> context)
+        {
+            //Burada StockRollbackMessage mesajını alıp OrderItems'larındaki ürünlerin stoklarını arttıracağız.
+            var stockCollection = mongoDbService.GetCollection<Stock.API.Models.Stock>();
+
+            //Eğer tüm ürünlerin stokları yeterli ise stokları düşüreceğiz ve StockReservedEvent mesajını göndereceğiz.
+            foreach (var orderItem in context.Message.OrderItems)
+            {
+                var stock = await (await stockCollection.FindAsync(x => x.ProductId == orderItem.ProductId)).FirstOrDefaultAsync();
+
+                stock.Count += orderItem.Count;
+                await stockCollection.FindOneAndReplaceAsync(x => x.ProductId == orderItem.ProductId, stock);
+            }
+        }
+    }
+}
