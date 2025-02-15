493ba8abb29dbb7431201f8a3ff2651f16081b2b
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2025-02-15 19:37:06 +0300

    update

---
+++ 5- Event-Sourcing/Microservices.Event.Store.Read.Data/Product.Application/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using EventStore.Client;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Product.Application.Models.ViewModels;
+using Shared.Events;
+using Shared.Services.Abstractions;
+
+namespace Product.Application.Controllers
+{
+    public class ProductsController(IEventStoreService eventStoreService) : Controller
+    {
+        // GET: ProductsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ProductsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductVM model)
+        {
+           await eventStoreService.AppendToStreamAsync("product-stream", new List<EventData>
+           {
+               eventStoreService.GenerateEventData(new NewProductAddedEvent
+               {
+                   ProductId = Guid.NewGuid().ToString(),
+                   ProductName = model.ProductName,
+                   InitialCount = model.Count,
+                   IsAvailable = model.IsAvailable,
+                   InitialPrice = model.Price
+               })
+           });
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
--- 5- Event-Sourcing/Microservices.Event.Store.Read.Data/Product.Application/Program.cs
+++ 5- Event-Sourcing/Microservices.Event.Store.Read.Data/Product.Application/Program.cs
@@ -1,9 +1,14 @@
+using Shared.Services;
+using Shared.Services.Abstractions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IEventStoreService, EventStoreService>();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -14,16 +19,14 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Products}/{action=Index}/{id?}");
 
 app.Run();
