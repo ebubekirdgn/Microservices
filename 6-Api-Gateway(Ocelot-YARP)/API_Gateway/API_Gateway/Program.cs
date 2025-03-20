a208269395563793cb28f41052d1803d4603a250
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2025-03-20 23:00:22 +0300

    update endpoint port number

--- 6-Api-Gateway(Ocelot-YARP)/API_Gateway/API_Gateway/Program.cs
+++ 6-Api-Gateway(Ocelot-YARP)/API_Gateway/API_Gateway/Program.cs
@@ -1,23 +1,18 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Configuration.AddJsonFile("ocelot.json");
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+await app.UseOcelot();
+app.UseHttpsRedirection();
 
 app.Run();
